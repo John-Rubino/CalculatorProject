18ea8707e46032d69066f64a042a84fd0785a652
John-Rubino <144836393+John-Rubino@users.noreply.github.com>
2023-12-18 21:36:15 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -6,6 +6,7 @@ def main():
     yes = True
     x = float(input('Enter a number: '))
     z = 0
+    ends = []
     while yes == True:
         if z >= 1:
             o = str(input('Enter an operator (type "h" for help, type "s" to stop the program): '))
@@ -23,96 +24,113 @@ def main():
             x = x + y
             print(f'The sum of {a} and {y} is {x}')
             z += 1
+            ends.append('+')
         elif o == '-':
             y = findy()
             a = x
             x = x - y
             print(f'The difference between {a} and {y} is {x}')
             z += 1
+            ends.append('-')
         elif o == '*' or o == 'x':
             y = findy()
             a = x
             x = x*y
             print(f'The product of {a} and {y} is {x}')
             z += 1
+            ends.append('*')
         elif o == '/':
             y = findy()
             a = x
             x = x/y
             print(f'The quotient of {a} and {y} is {x}')
             z += 1
+            ends.append('/')
         elif o == '%':
             y = findy()
             a = x
             x = x%y
             print(f'The remainder of {a} and {y} is {x}')
+            ends.append('%')
         elif o == '^':
             y = findy()
             a = x
             x = x**y
             print(f'{a}^{y} is equal to {x}')
             z += 1
-        elif o == 'q':
+            ends.append('^')
+        elif o == 'Q' or o == 'q':
             y = findy()
             b = y
             y = 1/y
             a = x
             x = x**y
             print(f'{b}q{a} is equal to {x}')
-        elif o == 'sin':
+            ends.append('Q')
+        elif o == 'Sin' or o == 'sin':
             a = x
             x = m.radians(x)
             x = m.sin(x)
             print(f'The sine of {a} is {x}')
             z += 1
-        elif o == 'cos':
+            ends.append('Sin')
+        elif o == 'Cos' or o == 'cos':
             a = x
             x = m.radians(x)
             x = m.cos(x)
             print(f'The cos of {a} is {x}')
             z += 1
-        elif o == 'tan':
+            ends.append('Cos')
+        elif o == 'Tan' or o == 'tan':
             a = x
             x = m.radians(x)
             x = m.tan(x)
             print(f'The tan of {a} is {x}')
             z += 1
-        elif o == 'asin':
+            ends.append('Tan')
+        elif o == 'Asin' or o == 'asin':
             a = x
             x = m.asin(x)
             x = m.degrees(x)
             print(f'The inverse sin of {a} is {x}')
             z += 1
-        elif o == 'acos':
+            ends.append('Asin')
+        elif o == 'Acos' or o == 'acos':
             a = x
             x = m.acos(x)
             x = m.degrees(x)
             print(f'The inverse cos of {a} is {x}')
             z += 1
-        elif o == 'atan':
+            ends.append('Acos')
+        elif o == 'Atan' or o == 'atan':
             a = x
             x = m.atan(x)
             x = m.degrees(x)
             print(f'The inverse tan of {a} is {x}')
             z += 1
-        elif o == 'log':
+            ends.append('Atan')
+        elif o == 'Log' or o == 'log':
             y = findy()
             if y == 0:
                 a = x
                 x = m.log(x, m.e)
                 print(f'The natural log of {a} is {x}')
                 z += 1
+                ends.append('Ln')
             else:
                 a = x
                 x = m.log(x, y)
                 print(f'The lof of {a} with a base {y} equals {x}')
                 z += 1
+                ends.append('Log')
         elif o == 's':
             yes = False
         else:
             print('Error, operator not recognized.')
+    fin = ' '.join(ends)
     print(f'The final number is {x}')
     print(f'A total of {z} operations were performed.')
+    print(f'A collected list of all the operations performed is as follows:\n{fin}.')
 
     return
 
